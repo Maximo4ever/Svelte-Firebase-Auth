26d61d7aafc6280dca7fba8b3e4cb58288098b6f
Maximo-4ever <maximilianocm06@gmail.com>
2022-05-14 09:39:50 -0500

    Agrega login con Google

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,2 +1,21 @@
+<script>
+  import { auth } from "../firebase";
+  import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+  import { isLoggedIn, user } from "../stores";
+  import { goto } from "$app/navigation";
+
+  const login = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const res = await signInWithPopup(auth, provider);
+      $user = res.user;
+      $isLoggedIn = true;
+      goto("/profile");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+</script>
+
 <h1>Login</h1>
-<button>Login with Google</button>
+<button on:click={login}>Login with Google</button>
--- src/routes/profile.svelte
+++ src/routes/profile.svelte
@@ -1,1 +1,10 @@
+<script>
+  import { isLoggedIn, user } from "../stores";
+</script>
+
 <h1>Profile</h1>
+{#if $isLoggedIn}
+  <img src={$user.photoURL} alt={user.displayName} />
+  <h3>{$user.displayName}</h3>
+  <p>{$user.email}</p>
+{/if}
