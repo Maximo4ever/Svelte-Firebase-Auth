113dc75cf7089b54ced1c6ada63b368ece6e585e
Maximo-4ever <maximilianocm06@gmail.com>
2022-05-14 09:09:42 -0500

    Añade la rutas iniciales

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<nav>
+  <ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/profile">Profile</a></li>
+    <li><a href="/login">Login</a></li>
+    <li><a href="/#">LogOut</a></li>
+  </ul>
+</nav>
+<slot />
---
+++ src/routes/login.svelte
@@ -0,0 +1,2 @@
+<h1>Login</h1>
+<button>Login with Google</button>
