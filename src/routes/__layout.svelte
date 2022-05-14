0d92b94d1dbcebb3349be6cdc09842b488bbcc71
Maximo-4ever <maximilianocm06@gmail.com>
2022-05-14 09:55:51 -0500

    Agrega el logout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,33 @@
+<script>
+  import { signOut, onAuthStateChanged } from "firebase/auth";
+  import { auth } from "../firebase";
+  import { isLoggedIn, user } from "../stores";
+
+  const logout = async () => {
+    try {
+      await signOut(auth);
+      $isLoggedIn = false;
+      $user = {};
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  onAuthStateChanged(auth, (authUser) => {
+    console.log(authUser);
+    $user = authUser;
+    $isLoggedIn = !!authUser;
+  });
+</script>
+
 <nav>
   <ul>
     <li><a href="/">Home</a></li>
-    <li><a href="/profile">Profile</a></li>
-    <li><a href="/login">Login</a></li>
-    <li><a href="/#">LogOut</a></li>
+    {#if $isLoggedIn}
+      <li><a href="/profile">Profile</a></li>
+      <li><a href="/#" on:click={logout}>LogOut</a></li>
+    {:else}
+      <li><a href="/login">Login</a></li>
+    {/if}
   </ul>
 </nav>
 <slot />
